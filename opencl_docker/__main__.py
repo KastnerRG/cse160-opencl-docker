d7770906716c0d3a78aab824717425d707f696b9
Christopher L. Crutchfield <ccrutchf@ucsd.edu>
2025-08-23 12:49:35 -0700

    Update __main__.py

--- opencl_docker/__main__.py
+++ opencl_docker/__main__.py
@@ -86,7 +86,7 @@ def install_oclgrind(dockerfile: Dockerfile):
     dockerfile.workdir("/oclgrind")
     dockerfile.run("git checkout v21.10 && mkdir build")
     dockerfile.workdir("/oclgrind/build")
-    dockerfile.run("cmake .. -DCMAKE_BUILD_TYPE=Release && \
+    dockerfile.run("cmake .. -DCMAKE_BUILD_TYPE=Release -DLLVM_DIR=/usr/lib/llvm-18/lib/cmake/llvm && \
                     make -j && \
                     make install && \
                     rm -rf /oclgrind")
