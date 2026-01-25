b7d0b8f0fec1c008ff15ebffc526f9e4f31f8bc0
Christopher L. Crutchfield <ccrutchf@ucsd.edu>
2026-01-24 16:36:49 -0800

    fix: replace NVIDIA_REQUIRE_CUDA to match 12.2.0

--- opencl_docker/__main__.py
+++ opencl_docker/__main__.py
@@ -101,6 +101,7 @@ def install_cuda_dsmlp(dockerfile: Dockerfile, args: Any):
         dockerfile.env(**{
             'CUDA_HOME' : "/usr/local/cuda",
             'CUDA_VERSION' : "12.2.0",
+            'NVIDIA_REQUIRE_CUDA': 'cuda>=12.2 brand=tesla,driver>=470,driver<471 brand=unknown,driver>=470,driver<471 brand=nvidia,driver>=470,driver<471 brand=nvidiartx,driver>=470,driver<471 brand=geforce,driver>=470,driver<471 brand=geforcertx,driver>=470,driver<471 brand=quadro,driver>=470,driver<471 brand=quadrortx,driver>=470,driver<471 brand=titan,driver>=470,driver<471 brand=titanrtx,driver>=470,driver<471 brand=tesla,driver>=525,driver<526 brand=unknown,driver>=525,driver<526 brand=nvidia,driver>=525,driver<526 brand=nvidiartx,driver>=525,driver<526 brand=geforce,driver>=525,driver<526 brand=geforcertx,driver>=525,driver<526 brand=quadro,driver>=525,driver<526 brand=quadrortx,driver>=525,driver<526 brand=titan,driver>=525,driver<526 brand=titanrtx,driver>=525,driver<526',
             'PATH' : '${CUDA_HOME}/bin:${PATH}'
         })
 
